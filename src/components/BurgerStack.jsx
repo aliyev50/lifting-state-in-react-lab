f6d212e99b78d55921acb03f90e4f1b93a90fcc7
cameron <aliyevcameron@gmail.com>
2024-07-18 23:51:20 -0400

    init commit

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BurgerStack = ({ stack, removeFromBurger }) => {
+  return (
+    <ul>
+      {stack.map((ingredient, index) => (
+        <li key={index} style={{ color: ingredient.color }}>
+          {ingredient.name} <button onClick={() => removeFromBurger(index)}>X</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default BurgerStack;
